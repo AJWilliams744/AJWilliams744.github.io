52a603688c5bc03a3b706826620ca234cb079f1e
A Williams <43878867+AJWilliams744@users.noreply.github.com>
2024-08-16 00:32:54 +0100

    Fixed about

---
+++ createNavigationBar.js
@@ -0,0 +1,18 @@
+window.onload = function () {
+    var navbarItems = `
+    <div class="title">
+        <div class="header">
+            <h3>Alex Williams</h3>
+            <h2>Portfolio Website</h2>
+        </div>
+        <div class="topnav">
+            <a href="index.html" class="nav">About</a>
+            <a class="nav">Released Games</a>
+            <a class="nav">Game Jams</a>
+            <a href="photoshop.html" class="nav"> Photoshop</a>
+            <a class="nav">Experimental / Tutorial Projects</a>
+        </div>
+    </div>  
+    `
+    document.getElementById("navbar-container").innerHTML = navbarItems;
+}
--- myscript.js
+++ myscript.js
@@ -1,3 +1,13 @@
-function changeColor() {
-    document.querySelector(".name").style.color = "blue";
-}
+// function getNavItems() {
+//     const navItems = document.querySelectorAll('.nav');
+//
+//     console.log(navItems)
+// }
+//
+//
+// // for (let i = 0; i < navItems.length; i++) {
+// //     navItems[i].addEventListener("click", function () {
+// //         Array.from(navItems, navItem => navItem.classList.remove('current'));
+// //         navItems[i].classList.add('current');
+// //     });
+// // }
